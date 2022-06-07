fc0c0ab2f13b8d548e921d0170343a5b2642817d
矢田桂都 <yatakeito@makkubukkuea.local>
2022-06-07 09:29:57 +0900

    daruma5

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text= ras))
+        TextSendMessage(text= ras()))
 
 
 if __name__ == "__main__":
