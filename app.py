50405dc0cd58c458ad880e3b098e3483dc98b436
矢田桂都 <yatakeito@makkubukkuea.local>
2022-06-08 14:54:21 +0900

    2７commit

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import scw 
+import os
+import requests
+import pprint
+import urllib.request
+from bs4 import BeautifulSoup
+
 
 line_bot_api = LineBotApi('bQcAlNXxwmjINfG8jFAdWcsAj8o0k9cseC1zPiLZyJgDAoJOpHjpiYOvs0wzLuvnbb7gQC9DChens1wBX0FyKtXKHfTPj/MZjfxJpxT44rsE7x1NWpfT4fTaszZ+qFT2TUYQdo+4F7XoOus+bj/24AdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('2ff241c4d4eb6e6c18018f4f8b6c43a5')
@@ -42,6 +49,13 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    
+    text_in = event.message.text
+    if "今日" in text_in: 
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=scw.getw()))
+    elif "明日" in text_in:   
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=scw.tom_getw()))
+    
     mondo = ras(event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
--- test10.py
+++ test10.py
@@ -27,6 +27,10 @@ def ras(message):
     
     elif text[0] == "5":
         ret= mondo()
+    
+    elif text[0] == "0":
+        ret= daruma()
+        
     else:
         ret="「数字,要件」で区切ってください"+\
             "1:予定登録, 2:予定確認, 3:天気, 4:ニュース, 5:出欠登録, 0:予定削除"
@@ -164,3 +168,5 @@ def mondo():
             "欠席:"+ "\n"+"[ "+ "https://forms.gle/ZgbsebUJgVLx8i3s7"+ " ]"+"\n"+\
             "AI開発情報サイト:"+ "\n"+ "[ "+ "https://sites.google.com/st.kobedenshi.ac.jp/it-info/%E3%82%AA%E3%83%B3%E3%83%A9%E3%82%A4%E3%83%B3%E6%8E%88%E6%A5%AD%E3%83%9D%E3%83%BC%E3%82%BF%E3%83%AB2022/ai%E3%83%86%E3%82%AF%E3%83%8E%E3%83%AD%E3%82%B8%E3%83%BC%E3%82%B3%E3%83%BC%E3%82%B9?authuser=0"+ " ]"
 
+def daruma():
+    return 
