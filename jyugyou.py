ead81189a2701efcb51370764d135ff8b3ac41aa
矢田桂都 <yatakeito@makkubukkuea.local>
2022-06-10 14:26:05 +0900

    36commit

--- jyugyou.py
+++ jyugyou.py
@@ -12,27 +12,27 @@ from pathlib import Path
 import os
 
 def getuyoubi():
-    return "1〜3限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/86778775630?pwd=dkVob0w0N3FnUitPWWMvdU9kY2hndz09"+"\n"+"\n"+\
-           "4〜5限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/86778775630?pwd=dkVob0w0N3FnUitPWWMvdU9kY2hndz09"+"\n"+"\n"+\
+    return "1〜3限"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/86778775630?pwd=dkVob0w0N3FnUitPWWMvdU9kY2hndz09"+"\n"+"\n"+\
+           "4〜5限"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/86778775630?pwd=dkVob0w0N3FnUitPWWMvdU9kY2hndz09"+"\n"+"\n"+\
            "<授業スライド>"+"\n"+"https://drive.google.com/drive/u/0/folders/0B1Mo1GBJNUCNa3pueEd5cDJwekU?resourcekey=0-udrE1VQAXJwBDyxlQbm2wA"+"\n"+"\n"+\
            "<実習提出フォルダ>"+"\n"+"https://drive.google.com/drive/u/0/folders/1BzgZiq3n-R6wOItdKYrXNDAfNjPhUQTO"
 
 def kayoubi():
-    return "1〜5限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
+    return "1〜5限"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
             "<授業スライド>"+"\n"+"https://drive.google.com/drive/folders/1GWGkc28e-nW4qMOGS7SNUzq4gM8M9qkc?lfhs=2"
 
 def suiyoubi():
-    return "1〜3限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
+    return "1〜3限"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
             "<授業スライド>"+"\n"+"https://drive.google.com/drive/folders/1SoOayO7TviKbtQreRf5WCYcR7ghIFx4B?lfhs=2"+"\n"+"\n"+\
-            "4〜5限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
+            "4〜5限"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
              "<授業スライド>"+"\n"+"https://drive.google.com/drive/folders/11Enl6ULkeLyFnchp2nWcGYWPlB-VspnR?lfhs=2"
              
 def mokuyoubi():
-    return "1〜3限"+"\n"+"\n"+ "<授業補足 ＆フォローアップ>"+"\n"+"https://paper.dropbox.com/doc/AI-20222023--BfH1BLF_WQHIiq3EA7MObdOqAQ-znaDm1RdpPKzQ2rmNJ4kG"+"\n"+"\n"+\
-           "4〜5限"+"\n"+"\n"+"<授業スライド>"+"\n"+"https://classroom.google.com/u/0/c/NDk5MzUzMTQ0Njk3"
+    return "1〜3限"+"\n"+"<授業補足 ＆フォローアップ>"+"\n"+"https://paper.dropbox.com/doc/AI-20222023--BfH1BLF_WQHIiq3EA7MObdOqAQ-znaDm1RdpPKzQ2rmNJ4kG"+"\n"+"\n"+\
+           "4〜5限"+"\n"+"<授業スライド>"+"\n"+"https://classroom.google.com/u/0/c/NDk5MzUzMTQ0Njk3"
 
 def kinyoubi():
-    return "1〜4限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
+    return "1〜4限"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
            "<授業スライド>"+"\n"+"https://classroom.google.com/u/0/c/NDk5MTg3MzYzMTU3?hl=ja"+"\n"+"\n"+\
-           "5限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
+           "5限"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/87860325168?pwd=bkkyZWZuNHpxNVZyQTdPVVlia2duZz09"+"\n"+"\n"+\
            "<授業スライド>"+"\n"+"https://classroom.google.com/u/0/c/NDk5MzUzMTQ0Njk3"
--- test10.py
+++ test10.py
@@ -107,8 +107,8 @@ def zeder():
     b = tenki+kion+low_kion+rain1+rain2+rain3+rain4
     return b
 def mondo():
-    return  "出席:"+ "\n"+"[ "+ "https://forms.gle/uXYUTBYEti5ofuRm6"+ " ]"+"\n"+\
-            "欠席:"+ "\n"+"[ "+ "https://forms.gle/ZgbsebUJgVLx8i3s7"+ " ]"+"\n"+\
+    return  "出席:"+ "\n"+"[ "+ "https://forms.gle/uXYUTBYEti5ofuRm6"+ " ]"+"\n"+"\n"+\
+            "欠席:"+ "\n"+"[ "+ "https://forms.gle/ZgbsebUJgVLx8i3s7"+ " ]"+"\n"+"\n"+\
             "AI開発情報サイト:"+ "\n"+ "[ "+ "https://sites.google.com/st.kobedenshi.ac.jp/it-info/%E3%82%AA%E3%83%B3%E3%83%A9%E3%82%A4%E3%83%B3%E6%8E%88%E6%A5%AD%E3%83%9D%E3%83%BC%E3%82%BF%E3%83%AB2022/ai%E3%83%86%E3%82%AF%E3%83%8E%E3%83%AD%E3%82%B8%E3%83%BC%E3%82%B3%E3%83%BC%E3%82%B9?authuser=0"+ " ]"
 def daruma():
     URL = "https://www.yahoo.co.jp/"
