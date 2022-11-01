7bc2bf9bbf3ae02917f277acbe9f7eac1ab53a87
矢田桂都 <yatakeito@makkubukkuea.local>
2022-11-01 10:03:01 +0900

    3

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from test10 import ras
-
 
 from flask import Flask, request, abort, render_template
 
@@ -12,34 +10,30 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-import glob
-import datetime
-import json
-import csv
+
+from test10 import suuji
 from dataclasses import replace
-import re
 import numpy as np
 from pathlib import Path
 import nyusu
-import scw 
-import os
-import requests
-import pprint
-import urllib.request
+import scw
 from bs4 import BeautifulSoup
-import test10 
+import test10
 import jyugyou
 import yotei
 
 
-line_bot_api = LineBotApi('EPobn2l7x8wn7aEGKrbZHqB/MMN2sHax2cd+blM2u0993YZcIIO2csK/Yjp+pUhiq6hCk3T6ez8buPtNSR49gQXAz/68TUu5D2MBIEYH74PU9/y6Zz5cNeQnaYCI21kmJRFrT5zszqaXj4emBGttywdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    'EPobn2l7x8wn7aEGKrbZHqB/MMN2sHax2cd+blM2u0993YZcIIO2csK/Yjp+pUhiq6hCk3T6ez8buPtNSR49gQXAz/68TUu5D2MBIEYH74PU9/y6Zz5cNeQnaYCI21kmJRFrT5zszqaXj4emBGttywdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('984e07ab505077e9d2c34e799d2ec905')
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def say_hello():
     return "Hello"
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -62,38 +56,44 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    
+
     text_in = event.message.text
-    if  "今日の天気" in text_in: 
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=scw.getw()))
-    elif "明日の天気" in text_in:   
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=scw.tom_getw()))
-    elif "ニュース" in text_in :
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=nyusu.nyu()))
+    if "今日の天気" in text_in:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=scw.getw()))
+    elif "明日の天気" in text_in:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=scw.tom_getw()))
+    elif "ニュース" in text_in:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=nyusu.nyu()))
     elif "出席" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=test10.mondo()))
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=test10.syusseki()))
     elif "月曜日" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=jyugyou.getuyoubi()))
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=jyugyou.getuyoubi()))
     elif "火曜日" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=jyugyou.kayoubi()))
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=jyugyou.kayoubi()))
     elif "水曜日" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=jyugyou.suiyoubi()))
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=jyugyou.suiyoubi()))
     elif "木曜日" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=jyugyou.mokuyoubi()))
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=jyugyou.mokuyoubi()))
     elif "金曜日" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=jyugyou.kinyoubi()))
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=jyugyou.kinyoubi()))
     elif "予定" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=yotei.yotei()))
-    elif "サブスク" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=test10.kawase()))
-    elif "なかやまきんに君" in text_in:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=test10.uruka()))
-    
-    mondo = ras(event.message.text)
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=yotei.yotei()))
+
+    x = suuji(event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text= mondo))
+        TextSendMessage(text=x))
 
 
 if __name__ == "__main__":
-    app.run()# Linebot
+    app.run()  # Linebot
--- test10.py
+++ test10.py
@@ -1,8 +1,6 @@
-import glob
+
 import datetime
 import requests
-import json
-import csv
 from dataclasses import replace
 import requests
 from bs4 import BeautifulSoup
@@ -12,66 +10,68 @@ from pathlib import Path
 import os
 import urllib.request
 
-def ras(message):
+
+def suuji(message):
     text = message.split(",", 1)
-    if text[0] =="1":
-        ret= mainy(text[1])
-    
-    elif text[0]=="2":
-        ret= cpt()
-    
+    if text[0] == "1":
+        ret = kakuninn(text[1])
+
+    elif text[0] == "2":
+        ret = touroku()
+
     elif text[0] == "3":
-        ret= selly()
-    
+        ret = tennki()
+
     elif text[0] == "4":
-        ret= zeder()
-    
+        ret = nexttennki()
+
     elif text[0] == "5":
-        ret= mondo()
-    
+        ret = syusseki()
+
     elif text[0] == "6":
-        ret= daruma()
-    
+        ret = news()
+
     elif text[0] == "7":
-        ret= arisaka()
-        
+        ret = jyugyou()
+
     else:
-        ret="出席、今日の天気、明日の天気、"+"\n"+"ニュース、(月〜金)曜日、予定"+"\n"+\
+        ret = "出席、今日の天気、明日の天気、"+"\n"+"ニュース、(月〜金)曜日、予定"+"\n" +\
             "のどれかを入力してください。"
-            
-        
+
     return ret
 
-def mainy(text):
-    yotei,day=text.split(",", 1)
-    day=day.split(",")
-    day= [int(d) for d in day]
+
+def kakuninn(text):
+    yotei, day = text.split(",", 1)
+    day = day.split(",")
+    day = [int(d) for d in day]
     dt1 = datetime.datetime.today()
-    dt2 = datetime.datetime(day[0],day[1],day[2])
+    dt2 = datetime.datetime(day[0], day[1], day[2])
     dt3 = dt2 - dt1
 
     return str(yotei) + "までは、後" + str(dt3.days) + "日後です"
 
-def cpt():
+
+def touroku():
     files = os.listdir("./yotei")
     for filename in files:
-        a= np.genfromtxt("yotei/" + filename, encoding='utf8', dtype=None)
-        aa=(f"{filename}:"+str(a).replace('"', ""))
-        return str(aa).replace(",","")
+        a = np.genfromtxt("yotei/" + filename, encoding='utf8', dtype=None)
+        aa = (f"{filename}:"+str(a).replace('"', ""))
+        return str(aa).replace(",", "")
 
-def selly():
-     #対象のサイトURL
+
+def tennki():
+    # 対象のサイトURL
     url = "https://tenki.jp/forecast/6/31/6310/28110/"
-    #インスタンス作成
+    # インスタンス作成
     res = urllib.request.urlopen(url)
     soup = BeautifulSoup(res, 'html.parser')
-    #対象の要素
+    # 対象の要素
     weather = soup.find_all("p", class_="weather-telop")
     temp = soup.find_all("dd", class_="high-temp temp")
     low_temp = soup.find_all("dd", class_="low-temp temp")
     tds = soup.select("tr.rain-probability td")
     hini = soup.find_all("h3", class_="left-style")
-
 
     tenki = hini[0].getText() + "\n\n" + weather[0].getText()
     kion = "\n最高 " + temp[0].getText()
@@ -80,16 +80,18 @@ def selly():
     rain2 = "\n06-12時  " + tds[1].getText()
     rain3 = "\n12-18時  " + tds[2].getText()
     rain4 = "\n18-24時  " + tds[3].getText()
-    
+
     a = tenki+kion+low_kion+rain1+rain2+rain3+rain4
-    return a 
-def zeder():
-    #対象のサイトURL
+    return a
+
+
+def nexttennki():
+    # 対象のサイトURL
     url = "https://tenki.jp/forecast/6/31/6310/28110/"
-    #インスタンス作成
+    # インスタンス作成
     res = urllib.request.urlopen(url)
     soup = BeautifulSoup(res, 'html.parser')
-    #対象の要素
+    # 対象の要素
     weather = soup.find_all("p", class_="weather-telop")
     temp = soup.find_all("dd", class_="high-temp temp")
     low_temp = soup.find_all("dd", class_="low-temp temp")
@@ -106,11 +108,15 @@ def zeder():
 
     b = tenki+kion+low_kion+rain1+rain2+rain3+rain4
     return b
-def mondo():
-    return  "出席:"+ "\n"+"[ "+ "https://forms.gle/uXYUTBYEti5ofuRm6"+ " ]"+"\n"+"\n"+\
-            "欠席:"+ "\n"+"[ "+ "https://forms.gle/ZgbsebUJgVLx8i3s7"+ " ]"+"\n"+"\n"+\
-            "AI開発情報サイト:"+ "\n"+ "[ "+ "https://sites.google.com/st.kobedenshi.ac.jp/it-info/%E3%82%AA%E3%83%B3%E3%83%A9%E3%82%A4%E3%83%B3%E6%8E%88%E6%A5%AD%E3%83%9D%E3%83%BC%E3%82%BF%E3%83%AB2022/ai%E3%83%86%E3%82%AF%E3%83%8E%E3%83%AD%E3%82%B8%E3%83%BC%E3%82%B3%E3%83%BC%E3%82%B9?authuser=0"+ " ]"
-def daruma():
+
+
+def syusseki():
+    return "出席:" + "\n"+"[ " + "https://forms.gle/uXYUTBYEti5ofuRm6" + " ]"+"\n"+"\n" +\
+        "欠席:" + "\n"+"[ " + "https://forms.gle/ZgbsebUJgVLx8i3s7" + " ]"+"\n"+"\n" +\
+        "AI開発情報サイト:" + "\n" + "[ " + "https://sites.google.com/st.kobedenshi.ac.jp/it-info/%E3%82%AA%E3%83%B3%E3%83%A9%E3%82%A4%E3%83%B3%E6%8E%88%E6%A5%AD%E3%83%9D%E3%83%BC%E3%82%BF%E3%83%AB2022/ai%E3%83%86%E3%82%AF%E3%83%8E%E3%83%AD%E3%82%B8%E3%83%BC%E3%82%B3%E3%83%BC%E3%82%B9?authuser=0" + " ]"
+
+
+def news():
     URL = "https://www.yahoo.co.jp/"
     rest = requests.get(URL)
 
@@ -120,23 +126,15 @@ def daruma():
     # ヤフーニュースの見出しとURLの情報を取得して出力する
     data_list = soup.find_all(href=re.compile("news.yahoo.co.jp/pickup"))
     for data in data_list:
-        a= data.span.string 
-        b= data.attrs["href"]
-        x= a+ b
+        a = data.span.string
+        b = data.attrs["href"]
+        x = a + b
     return x
 
-def arisaka():
-    return "1〜3限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/86778775630?pwd=dkVob0w0N3FnUitPWWMvdU9kY2hndz09"+"\n"+"\n"+\
-           "4〜5限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/86778775630?pwd=dkVob0w0N3FnUitPWWMvdU9kY2hndz09"+"\n"+"\n"+\
-           "<授業スライド>"+"\n"+"https://drive.google.com/drive/u/0/folders/0B1Mo1GBJNUCNa3pueEd5cDJwekU?resourcekey=0-udrE1VQAXJwBDyxlQbm2wA"+"\n"+"\n"+\
-           "<実習提出フォルダ>"+"\n"+"https://drive.google.com/drive/u/0/folders/1BzgZiq3n-R6wOItdKYrXNDAfNjPhUQTO"
 
-def kawase():
-    return "[Netflix]"+"\n"+"https://www.netflix.com/browse"+"\n"+"\n"+\
-           "[Amazon Prime]"+"\n"+"https://www.amazon.co.jp/gp/video/storefront/ref=topnav_storetab_aiv?node=2351649051"+"\n"+"\n"+\
-           "[Disney+]"+"\n"+"https://www.disneyplus.com/ja-jp/login"+"\n"+"\n"+\
-           "[dTV]"+"\n"+"https://video.dmkt-sp.jp/?cnt=hn000065"
-
-def uruka():
-    return "ヤー！パワー！！！"+"\n"+"\n"+\
-           "https://www.youtube.com/channel/UCOUu8YlbaPz0W2TyFTZHvjA/videos?view=57"
+def jyugyou():
+    return "1〜3限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/86778775630?pwd=dkVob0w0N3FnUitPWWMvdU9kY2hndz09"+"\n"+"\n" +\
+           "4〜5限"+"\n"+"\n"+"<zoom>"+"\n"+"https://us02web.zoom.us/j/86778775630?pwd=dkVob0w0N3FnUitPWWMvdU9kY2hndz09"+"\n"+"\n" +\
+           "<授業スライド>"+"\n"+"https://drive.google.com/drive/u/0/folders/0B1Mo1GBJNUCNa3pueEd5cDJwekU?resourcekey=0-udrE1VQAXJwBDyxlQbm2wA"+"\n"+"\n" +\
+           "<実習提出フォルダ>"+"\n" + \
+        "https://drive.google.com/drive/u/0/folders/1BzgZiq3n-R6wOItdKYrXNDAfNjPhUQTO"
